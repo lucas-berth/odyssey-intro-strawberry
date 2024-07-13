4cf9a31a2465828c0a4cc75053573de1cebd561f
lucasberth <lucas.berth529@gmail.com>
2024-07-12 23:37:42 -0500

    1 got the api client working after just trying the execute code many times 2 got the new query setup. App is working as of this push

--- api/query.py
+++ api/query.py
@@ -1,5 +1,7 @@
 import strawberry
 from .types.playlist import Playlist
+from mock_spotify_rest_api_client.api.playlists import get_featured_playlists
+from mock_spotify_rest_api_client.api.playlists import get_playlist
 
 #resovler or in OOP terms this is our "getter"
 #def get_hello():
@@ -9,12 +11,39 @@ from .types.playlist import Playlist
 @strawberry.type
 class Query:
     @strawberry.field(description="Playlists hand-picked to be features to all users")
-    def features_playlists(self) -> list[Playlist]:
-        return [
-            Playlist(id="1", name="GraphQL Groovin'", description=None),
-            Playlist(id="2", name="Graph Explorer Jams", description=None),
-            Playlist(id="3", name="Interpretive GraphQL Dance", description=None),
+    async def features_playlists(self, info: strawberry.Info) -> list[Playlist]:
+        spotify_client = info.context["spotify_client"]
+        data = await get_featured_playlists.asyncio(client=spotify_client)
+        
+        return  [
+            Playlist(
+                id=strawberry.ID(playlist.id),
+                name=playlist.name,
+                description=playlist.description
+            )
+            for playlist in data.playlists.items
         ]
+    
+
+    @strawberry.field(description="Retrieves a specific playlist.")
+    async def playlist(self,
+            id: strawberry.ID,
+            info: strawberry.Info
+            ) -> Playlist | None:
+                spotify_client = info.context["spotify_client"]
+                data = await get_playlist.asyncio(client=spotify_client, playlist_id=id)
+                
+                if data is None:
+                        return None
+                
+                return Playlist(
+                        id=strawberry.ID(data.id),
+                        name=data.name,
+                        description=data.description
+                )
+        
+
+
     #hello: str = strawberry.field(resolver=get_hello)
 
 
